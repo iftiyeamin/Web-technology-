da1dab9139de3c438817e5b63de753fe2ecabcdb
iftiyeamin <iftiyeamin08@gmail.com>
2022-03-12 23:59:33 +0600

    first commit

---
+++ Labtask4/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<img align="left"  width="40" height="40"> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+<?php
+
+$login = "login.php";
+$registration = "registration.php";
+$logout = "logout.php";
+$self = "";
+
+
+
+?>
+</body>
+</html>
